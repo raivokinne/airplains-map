a813e4762ca742760e3e016ea5fc73b396c6a22b
RaivoKinne <120594466+raivokinne@users.noreply.github.com>
2024-10-15 09:49:14 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 
 import Map from "@/components/Map";
+import useFetchPlainData from "@/hooks/useFetchPlainData";
 
 export default function Home() {
+
+	const { data, loading, error } = useFetchPlainData();
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error: {error.message}</p>;
+
+	console.log(data)
 	return (
 		<>
-			<Map/>
+			<Map />
 		</>
 	)
 }
---
+++ hooks/useFetchPlainData.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function useFetchPlainData() {
+
+	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState<Error | null>(null);
+
+	useEffect(() => {
+		async function fetchData() {
+			setLoading(true);
+			setError(null);
+
+			try {
+				const response = await fetch('https://opensky-network.org/api/states/all');
+				const data = await response.json();
+				setData(data);
+			} catch (error) {
+				if (error instanceof Error) {
+					setError(error);
+				}
+			} finally {
+				setLoading(false);
+			}
+		}
+
+		fetchData();
+	}, []);
+
+	return { data, loading, error };
+}
+
